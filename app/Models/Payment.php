68fe4c281ebb4185b129333bdf7cc2296516d60b
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-05-02 23:08:21 -0400

    add new methods for paymnest

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,14 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Plan extends Model
+class Payment extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name', 
-        'description', 
-        'monthly_cost', 
-        'speed', 
-        'data_limit'
+        'client_id', 
+        'amount', 
+        'payment_date', 
+        'payment_method'
     ];
 }
