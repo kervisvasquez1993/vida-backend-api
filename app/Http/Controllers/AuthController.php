f277a25856ac11d63b4ae31bcf74bb34c3dfa2a8
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-09-04 17:54:36 -0400

    feat:registro de user con su perfil completado, se agrego la funcionalidad en el controlador para porder almacenar los datos en la tabla de user y de profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\UserResource;
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
 
 class AuthController extends Controller
 {
@@ -54,35 +57,36 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'username' => 'required|unique:users',
+            'email' => 'required|unique:users',
+            'role' => 'required|in:admin,client,tecnicos',
+            'password' => 'required|min:6|confirmed',
             'name' => 'required',
             'last_name' => 'required',
             'phone' => 'required',
             'address' => 'required',
-            'email' => 'required|unique:users',
-            'password' => 'required|min:6|confirmed',
-
-
+            'date_of_birth' => 'required',
+            'gender' => 'required|in:hombre,mujer'
         ]);
-
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 422);
         }
-
-        $user = new User();
-        $user->username = str_replace(' ', '_', $request->username);
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->role = "patient";
-        $user->save();
-        $user->profile()->create([
-            'name' => $request->name,
-            'last_name' => $request->last_name,
-            'phone' => $request->phone,
-            'address' => $request->address
-        ]);
-
-
-        $user->save();
-        return response()->json(['message' => "Hola, $user->username tu registro fue completado"], 201);
+        DB::beginTransaction();
+        try {
+            $user = new User();
+            $user->username = str_replace(' ', '_', $request->username);
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->role = $request->role;
+            $user->save();
+            $user_id = auth()->user()->id;
+            $request->merge(['user_id' => $user_id]);
+            $profile = Profile::create($request->all());
+            DB::commit();
+            return response()->json(['data' => $profile]);
+            return response()->json(['message' => "Hola, $user->username tu registro fue completado"], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => 'Error en el registro, por favor intenta nuevamente.'], 500);
+        }
     }
 }
