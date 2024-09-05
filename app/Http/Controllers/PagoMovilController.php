e3378cd4e277fe640481560942a86ea48d01e78f
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-09-05 19:15:27 -0400

    add: se agrego controlador para agregar la logica del pago movil

---
+++ app/Http/Controllers/PagoMovilController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PagoMovil;
+use Illuminate\Http\Request;
+
+class PagoMovilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PagoMovil $pagoMovil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PagoMovil $pagoMovil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PagoMovil $pagoMovil)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PagoMovilController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,6 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/me', [AuthController::class, 'me'])->name('me');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
-    
+    // modulo para registrar bancos
+    Route::post('/pago-movil', [PagoMovilController::class, 'store'])->name('store');
 });
