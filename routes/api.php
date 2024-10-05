9bca1a4f30326a8f279a0eb13455b7b7afb42a65
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-10-05 13:41:16 -0300

    add new route in backend

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\PagoMovilController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\PlanController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -34,5 +36,20 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/clientes/{id}', [ClientController::class, 'show'])->name('show');
     Route::put('/clientes/{id}', [ClientController::class, 'update'])->name('update');
     Route::delete('/clientes/{id}', [ClientController::class, 'destroy'])->name('destroy');
-    
+
+    // Payments
+    Route::get('/payments', [PaymentController::class, 'index'])->name('index.payments');
+    Route::get('/payments/{id}', [PaymentController::class, 'show'])->name('show.payment');
+    Route::post('/payments', [PaymentController::class, 'store'])->name('store.payments');
+    Route::put('/payments/{id}', [PaymentController::class, 'update'])->name('put.payment');
+    Route::delete('/payments/{id}', [PaymentController::class, 'delete'])->name('delete.payment');
+    Route::post('/payments/verify', [PaymentController::class, 'verify'])->name('verify.payments');
+
+    // transaction
+
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('index.transactions');
+    Route::get('/transactions/{id}', [TransactionController::class, 'show'])->name('show.transactions');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('store.transactions');
+    Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('update.transactions');
+    Route::delete('/transactions/{id}', [TransactionController::class, 'delete'])->name('delete.transactions');
 });
